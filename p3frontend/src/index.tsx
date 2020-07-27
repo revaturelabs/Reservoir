b9248cf4667e2d5d1ccded77e41bca24a124ef77
thetedyoung <65968091+thetedyoung@users.noreply.github.com>
2020-07-27 13:54:43 -0700

    Revert "added first round of tests"

--- p3frontend/src/index.tsx
+++ p3frontend/src/index.tsx
@@ -4,20 +4,15 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Spinner } from "./Components/Common/spinner";
-import { createStore } from 'redux';
-import { batchReducer } from "./redux/reducers"
-import { Provider } from "react-redux";
 
-const store = createStore(batchReducer);
+
 
 ReactDOM.render(
-  <Provider store = {store}>
   <React.StrictMode>
     
     <App />
     <Spinner area="invalid"/>
-  </React.StrictMode>
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
